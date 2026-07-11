3ab3de48b0cb647e87b177ea65d7fbb0e52cde0e
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-11 18:01:35 +0800

    Added declarations

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/FileWordRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/FileWordRepository.java
@@ -1,4 +1,8 @@
 package ph.edu.dlsu.lbycpob.hangman.repository;
 
-public class FileWordRepository {
-}
+import java.util.Random;
+
+public final class FileWordRepository implements WordRepository {
+    private final Random random;
+
+
