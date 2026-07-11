78acc8db05d381729f5625c93199047e7f7bd23a
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-11 14:30:42 +0800

    Added declarations

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/game/HangmanGame.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/game/HangmanGame.java
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.lbycpob.hangman.game;
 
 public interface HangmanGame {
+    void run();
+    void intro();
+    int playOneGame(String secretWord);
+    void displayHangman(int guessCount);
+    String createHint(String secretWord, String guessedLetters);
+    char readGuess(String guessedLetters);
+    String getRandomWord(String filename);
+    void stats(int gamesCount, int gamesWon, int best);
 }
