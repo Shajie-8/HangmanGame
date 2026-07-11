5aafe121fe5276d43cf3e7dfe03e806d4f8c24b8
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-11 18:02:07 +0800

    Added FileWordRepository method

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/FileWordRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/FileWordRepository.java
@@ -1,8 +1,16 @@
 package ph.edu.dlsu.lbycpob.hangman.repository;
 
+import java.util.Objects;
 import java.util.Random;
 
 public final class FileWordRepository implements WordRepository {
     private final Random random;
+
+    // UNDERSTAND: Instantiates a physical disk file searcher.
+    public FileWordRepository(Random random) {
+        this.random = Objects.requireNonNull(random, "random must not be null");
+    }
+
+
 
 
