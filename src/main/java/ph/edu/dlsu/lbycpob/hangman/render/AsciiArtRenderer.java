055eb1407985daefad81e1b1ffdb3e09572cf843
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-11 17:21:44 +0800

    Added declarations

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/render/AsciiArtRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/render/AsciiArtRenderer.java
@@ -1,4 +1,8 @@
 package ph.edu.dlsu.lbycpob.hangman.render;
 
-public class AsciiArtRenderer {
+public class AsciiArtRenderer implements HangmanRenderer {
+    // UNDERSTAND: Constants to define boundary restrictions on guesses.
+    private static final int MIN_GUESSES_REMAINING = 0;
+    private static final int MAX_GUESSES_REMAINING = 8;
+    private final String resourceBasePath;
 }
