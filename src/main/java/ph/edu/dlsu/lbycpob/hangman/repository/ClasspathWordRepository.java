bb1ce6b25a92424a051e3f839871da2911546310
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-11 17:58:28 +0800

    Added declarations

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/ClasspathWordRepository.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/repository/ClasspathWordRepository.java
@@ -1,4 +1,7 @@
 package ph.edu.dlsu.lbycpob.hangman.repository;
 
-public class ClasspathWordRepository {
-}
+import java.util.Random;
+
+public final class ClasspathWordRepository implements WordRepository {
+    private final String resourceBasePath;
+    private final Random random;
