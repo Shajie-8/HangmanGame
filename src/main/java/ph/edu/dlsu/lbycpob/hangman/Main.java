90ddd3323c1afb70ab33fd7cfd45eb66c273606c
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-12 19:51:53 +0800

    Adding WordRepository main method

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/Main.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/Main.java
@@ -6,6 +6,7 @@ import ph.edu.dlsu.lbycpob.hangman.render.HangmanRenderer;
 import ph.edu.dlsu.lbycpob.hangman.repository.ClasspathWordRepository;
 import ph.edu.dlsu.lbycpob.hangman.repository.WordRepository;
 
+import java.io.File;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,3 +22,11 @@ public class Main {
 
         Hangman introGame = new Hangman(renderer, dummyRepo, random, "");
         introGame.intro();
+
+        // UNDERSTAND: Input/Output operation reading the dictionary file path selection from the user.
+        System.out.print("Enter the word list filename (test.txt, words.txt, or large.txt): ");
+        String filename = scanner.nextLine().trim();
+
+        WordRepository repository;
+        // UNDERSTAND: Variable declaration of the local File instance to check system file presence.
+        File localFile = new File(filename);
