5bc1f21b630fd6dd59461d2a2b37b01bdb3b3803
Shajie-8 <shajie.macabalang@gmail.com>
2026-07-12 19:51:06 +0800

    Implementing main method

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/Main.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/Main.java
@@ -1,4 +1,23 @@
 package ph.edu.dlsu.lbycpob.hangman;
 
+import ph.edu.dlsu.lbycpob.hangman.game.Hangman;
+import ph.edu.dlsu.lbycpob.hangman.render.AsciiArtRenderer;
+import ph.edu.dlsu.lbycpob.hangman.render.HangmanRenderer;
+import ph.edu.dlsu.lbycpob.hangman.repository.ClasspathWordRepository;
+import ph.edu.dlsu.lbycpob.hangman.repository.WordRepository;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     private static final String GAME_ASSETS_BASE_PATH = "/game-assets";
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        HangmanRenderer renderer = new AsciiArtRenderer(GAME_ASSETS_BASE_PATH + "/hangman-art");
+        WordRepository dummyRepo = new ClasspathWordRepository(GAME_ASSETS_BASE_PATH + "/words", random);
+
+        Hangman introGame = new Hangman(renderer, dummyRepo, random, "");
+        introGame.intro();
